187905c7a6347ff103ee2734a38861fc78462db4
Andrei Ahureikin <Andrei.Ahureikin@itechart-group.com>
2022-01-04 13:41:53 +0300

    RL-4: fix error message

--- server/middleware/auth.middleware.ts
+++ server/middleware/auth.middleware.ts
@@ -10,7 +10,7 @@ const authMiddleware = (req, res, next) => {
         const token = req.headers.authorization.split(' ')[1]
 
         if (!token) {
-            return res.status(401).json({ message: 'Auth error123' })
+            return res.status(401).json({ message: 'Auth error' })
         }
 
         const decoded = jwt.verify(token, SECRET_KEY)
